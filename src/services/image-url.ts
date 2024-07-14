507c0e599c4ea61b835450da82c3953f35e4de94
Rohan Peddamallu <rohanpeddamallu@gmail.com>
2024-07-13 21:48:04 -0500

    sort games.

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,8 +1,10 @@
-const getCroppedImageURL = (url: string) => {
+import noImage from '../assets/GameHub Resources/Image Placeholder/no-image-placeholder-6f3882e0.webp';
+const getCroppedImageUrl = (url: string) => {
+  if (!url) return noImage;
+  
   const target = 'media/';
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
 }
 
-
-export default getCroppedImageURL;
+export default getCroppedImageUrl;
